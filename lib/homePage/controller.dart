6f447dc962103cb4bc05bc43b6ee614765096557
Muhammad Zain <zainmughal4379329@gmail.com>
2024-02-12 19:19:54 +0500

    places feteched

--- lib/homePage/controller.dart
+++ lib/homePage/controller.dart
@@ -1,10 +1,60 @@
 
 
 
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:yb_ride_user_web/helper/AppConstants.dart';
 import 'package:yb_ride_user_web/homePage/state.dart';
+import 'package:http/http.dart' as http;
+import 'package:geocoding/geocoding.dart';
 
 class HomePageCon extends GetxController{
   final state = HomePageState();
 
+  void getSuggestions(
+      String input, dynamic sessionToken, HomePageCon controller) async {
+    print("get suggestions called");
+    String baseURL = AppConstants.placesBaseUrl;
+    String kPLACES_API_KEY = AppConstants.kPlacesApiKey;
+    String request =
+        '$baseURL?input=$input&key=$kPLACES_API_KEY&sessiontoken=$sessionToken';
+
+    var response = await http.get(Uri.parse(request)).then((response) {
+      print(response.toString());
+      if (response.statusCode == 200) {
+        List predictions = jsonDecode(response.body.toString())['predictions'];
+        List bostonPredictions = predictions.where((prediction) {
+          return prediction['description'].toLowerCase().contains('boston');
+        }).toList();
+
+        state.results.value = bostonPredictions.length;
+        // cont.state.placeList.value =
+        controller.state.placeList.value = bostonPredictions;
+        print(bostonPredictions.toString());
+
+      } else {
+        print("status code");
+        print(response.statusCode);
+
+      }
+    }).onError((error, stackTrace) {
+      print(error.toString());
+    });
+  }
+
+
+  void GetCoordinates(BuildContext context) async {
+
+    List<Location> coordinates =
+    await locationFromAddress(state.selectedPlace.toString());
+    if (coordinates.isNotEmpty) {
+      final lat = coordinates.first;
+      state.fromLat.value = lat.latitude;
+      state.fromLang.value = lat.longitude;
+      // state.loc.value = LatLng(lat.latitude, lat.longitude);
+      // animationOfCamera(state.loc.value);
+    }
+  }
 }
--- lib/homePage/state.dart
+++ lib/homePage/state.dart
@@ -2,7 +2,10 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:uuid/uuid.dart';
+import 'package:geocoding/geocoding.dart';
 
 class HomePageState{
 
@@ -15,4 +18,142 @@ class HomePageState{
   final toController = TextEditingController();
   final isSwitchButtonVal =false.obs;
   final isButtonVisible = false.obs;
+
+
+
+  //Places Variables
+  Uuid? uuid ;
+  String? sessionToken;
+  final locationSearchController = TextEditingController();
+  RxInt results = 0.obs;
+  RxList placeList = [].obs;
+  RxBool searchLoading = false.obs;
+  RxBool locationSelected = false.obs;
+  // var loc = LatLng(42.361145, -71.057083).obs;
+
+
+  RxString selectedPlace = 'Tap to Search'.obs;
+  RxString returnPlace = 'Return Place'.obs;
+  RxDouble fromLat = 0.0.obs;
+  RxDouble fromLang = 0.0.obs;
+  RxDouble toLat = 0.0.obs;
+  RxDouble toLang = 0.0.obs;
+
+  RxString fromMonth = "".obs;
+  RxString fromMonthName = "".obs;
+  RxString fromDate = "".obs;
+  RxString fromDateName = "".obs;
+  RxString fromYear = "".obs;
+  RxString fromTime = "11:00 AM".obs;
+  RxString toMonth = "".obs;
+  RxString toMonthName = "".obs;
+  RxString toDate = "".obs;
+  RxString toDateName = "".obs;
+  RxString toYear = "".obs;
+  RxString toTime = "11:00 AM".obs;
+  String fromTimeinMiliSeconds = "";
+  String toTimeinMiliSeconds = "";
+  final items = <DropdownMenuItem>[
+    DropdownMenuItem(
+      child: Text('7:30 AM'),
+      value: '7:30 AM',
+    ),DropdownMenuItem(
+      child: Text('8:00 AM'),
+      value: '8:00 AM',
+    ),
+    DropdownMenuItem(
+      child: Text('8:30 AM'),
+      value: '8:30 AM',
+    ),
+    DropdownMenuItem(
+      child: Text('9:00 AM'),
+      value: '9:00 AM',
+    ),
+    DropdownMenuItem(
+      child: Text('9:30 AM'),
+      value: '9:30 AM',
+    ),DropdownMenuItem(
+      child: Text('10:00 AM'),
+      value: '10:00 AM',
+    ),DropdownMenuItem(
+      child: Text('10:30 AM'),
+      value: '10:30 AM',
+    ),DropdownMenuItem(
+      child: Text('11:00 AM'),
+      value: '11:00 AM',
+    ),DropdownMenuItem(
+      child: Text('11:30 AM'),
+      value: '11:30 AM',
+    ),DropdownMenuItem(
+      child: Text('12:00 PM'),
+      value: '12:00 PM',
+    ),DropdownMenuItem(
+      child: Text('12:30 PM'),
+      value: '12:30 PM',
+    ),DropdownMenuItem(
+      child: Text('1:00 PM'),
+      value: '1:00 PM',
+    ),DropdownMenuItem(
+      child: Text('1:30 PM'),
+      value: '1:30 PM',
+    ),DropdownMenuItem(
+      child: Text('2:00 PM'),
+      value: '2:00 PM',
+    ),DropdownMenuItem(
+      child: Text('2:30 PM'),
+      value: '2:30 PM',
+    ),DropdownMenuItem(
+      child: Text('3:00 PM'),
+      value: '3:00 PM',
+    ),DropdownMenuItem(
+      child: Text('3:30 PM'),
+      value: '3:30 PM',
+    ),
+    DropdownMenuItem(
+      child: Text('4:00 PM'),
+      value: '4:00 PM',
+    ),DropdownMenuItem(
+      child: Text('4:30 PM'),
+      value: '4:30 PM',
+    ),DropdownMenuItem(
+      child: Text('5:00 PM'),
+      value: '5:00 PM',
+    ),DropdownMenuItem(
+      child: Text('5:30 PM'),
+      value: '5:30 PM',
+    ),DropdownMenuItem(
+      child: Text('6:00 PM'),
+      value: '6:00 PM',
+    ),DropdownMenuItem(
+      child: Text('6:30 PM'),
+      value: '6:30 PM',
+    ),DropdownMenuItem(
+      child: Text('7:00 PM'),
+      value: '7:00 PM',
+    ),DropdownMenuItem(
+      child: Text('7:30 PM'),
+      value: '7:30 PM',
+    ),DropdownMenuItem(
+      child: Text('8:00 PM'),
+      value: '8:00 PM',
+    ),DropdownMenuItem(
+      child: Text('8:30 PM'),
+      value: '8:30 PM',
+    ),DropdownMenuItem(
+      child: Text('9:00 PM'),
+      value: '9:00 PM',
+    ),DropdownMenuItem(
+      child: Text('9:30 PM'),
+      value: '9:30 PM',
+    ),DropdownMenuItem(
+      child: Text('10:00 PM'),
+      value: '10:00 PM',
+    ),DropdownMenuItem(
+      child: Text('10:30 PM'),
+      value: '10:30 PM',
+    ),DropdownMenuItem(
+      child: Text('11:00 PM'),
+      value: '11:00 PM',
+    ),
+  ];
 }
